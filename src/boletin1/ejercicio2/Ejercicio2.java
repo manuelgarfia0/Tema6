55de3e338e2ef14976cd2baad9831d206036a9c9
manuelgarfia0 <manuel.garfia@iesnervion.es>
2025-04-22 14:40:22 +0200

    Hasta ejercicio 4

---
+++ src/boletin1/ejercicio2/Ejercicio2.java
@@ -0,0 +1,35 @@
+package boletin1.ejercicio2;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+
+		int suma = 0;
+		double media;
+		int num;
+		int cont = 0;
+
+		try {
+			Scanner scanner = new Scanner(new FileReader("src\\boletin1\\ejercicio2\\Enteros.txt"));
+			while (scanner.hasNextInt()) {
+				num = scanner.nextInt();
+				suma += num;
+				cont++;
+			}
+
+			media = suma / cont;
+			System.out.println("Suma = " + suma);
+			System.out.println("Media = " + media);
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("El fichero: " + e.getMessage());
+		}
+
+	}
+
+}
